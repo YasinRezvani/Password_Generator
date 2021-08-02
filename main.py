5e94d9eb9b09b1d861ec0bcf91d6685934b99b42
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-02 10:30:27 +0430

    define loop for chosse char in data and put to password with random.choice

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 ﻿import random
+
+char = "zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLPOIUYTREWQ1234567890@#$%&*."
+
+lens = int(input("What is the length of the password: "))
+print("\nYour Password:")
+print("---------------------------------")
+
+for pas in range(0 , 5):
+    password = ""
+    for l in range(lens):
+        password += random.choice(char)
+       
+    print(password)    
+
+print("---------------------------------")
+
+# Made By Yasin Rezvani
